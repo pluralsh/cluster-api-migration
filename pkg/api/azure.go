ba342bc631342bb4b691a8eb8d18c6a4fa0f48cd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-05-29 11:26:59 +0200

    Add comments

--- pkg/api/azure.go
+++ pkg/api/azure.go
@@ -4,22 +4,47 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+// Following specs should match ones from plural-artifacts repository.
+
 type Tags map[string]string
 
 type IdentityType string
 
 type AzureCloudSpec struct {
-	ClusterIdentityName string                 `json:"clusterIdentityName,omitempty"`
-	ClusterIdentityType IdentityType           `json:"clusterIdentityType,omitempty"`
-	ClientID            string                 `json:"clientID"`
-	ClientSecret        corev1.SecretReference `json:"clientSecret,omitempty"`
-	ClientSecretName    string                 `json:"clientSecretName,omitempty"`
-	ResourceID          string                 `json:"resourceID,omitempty"`
-	TenantID            string                 `json:"tenantID"`
-	SubscriptionID      string                 `json:"subscriptionID"`
-	Location            string                 `json:"location"`
-	ResourceGroupName   string                 `json:"resourceGroupName"`
-	SSHPublicKey        string                 `json:"sshPublicKey"`
+	// Name of AzureClusterIdentity to be used when reconciling this cluster.
+	ClusterIdentityName string `json:"clusterIdentityName,omitempty"`
+
+	// Type of Azure Identity used.
+	// One of: ServicePrincipal, ServicePrincipalCertificate, UserAssignedMSI or ManualServicePrincipal.
+	ClusterIdentityType IdentityType `json:"clusterIdentityType,omitempty"`
+
+	// Service Principal client ID. Both Service Principal and User Assigned MSI can use this field.
+	ClientID string `json:"clientID"`
+
+	// Service Principal password.
+	ClientSecret corev1.SecretReference `json:"clientSecret,omitempty"`
+
+	// Name of Secret containing clientSecret.
+	ClientSecretName string `json:"clientSecretName,omitempty"`
+
+	// Azure resource ID for the User Assigned MSI resource.
+	ResourceID string `json:"resourceID,omitempty"`
+
+	// Service Principal primary tenant ID.
+	TenantID string `json:"tenantID"`
+
+	// GUID of the Azure subscription to hold this cluster.
+	SubscriptionID string `json:"subscriptionID"`
+
+	// String matching one of the canonical Azure region names.
+	// Examples: "westus2", "eastus".
+	Location string `json:"location"`
+
+	// Name of the Azure resource group for this AKS Cluster.
+	ResourceGroupName string `json:"resourceGroupName"`
+
+	// String literal containing an SSH public key base64 encoded.
+	SSHPublicKey string `json:"sshPublicKey"`
 }
 
 type AzureDefaultWorker struct {
