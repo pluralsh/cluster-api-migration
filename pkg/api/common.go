22a3c608b91706ffc245f66dd7618608c8fefc91
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-05-30 17:24:10 +0200

    Update Azure worker defaults

--- pkg/api/common.go
+++ pkg/api/common.go
@@ -1,7 +1,11 @@
 package api
 
-// ManagedMachinePoolScaling specifies scaling options.
 type ManagedMachinePoolScaling struct {
 	MinSize int32 `json:"minSize"`
 	MaxSize int32 `json:"maxSize"`
 }
+
+const (
+	LinuxOS   = "Linux"
+	WindowsOS = "Windows"
+)
