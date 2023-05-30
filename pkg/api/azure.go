021b3f459bfea9dcb735d2ed4976424961baa41b
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-05-30 14:58:34 +0200

    Update Azure worker type

--- pkg/api/azure.go
+++ pkg/api/azure.go
@@ -2,10 +2,6 @@ package api
 
 // Following specs should match ones from plural-artifacts repository.
 
-type Tags map[string]string
-
-type IdentityType string
-
 type AzureCloudSpec struct {
 	// Name of AzureClusterIdentity to be used when reconciling this cluster.
 	ClusterIdentityName string `json:"clusterIdentityName,omitempty"`
@@ -53,15 +49,79 @@ type AzureWorker struct {
 }
 
 type AzureWorkerSpec struct {
-	AdditionalTags    Tags                       `json:"additionalTags,omitempty"`
-	Mode              string                     `json:"mode"`
-	SKU               string                     `json:"sku"`
-	OSDiskSizeGB      *int32                     `json:"osDiskSizeGB,omitempty"`
-	AvailabilityZones []string                   `json:"availabilityZones,omitempty"`
-	NodeLabels        map[string]string          `json:"nodeLabels,omitempty"`
-	Taints            Taints                     `json:"taints,omitempty"`
-	Scaling           *ManagedMachinePoolScaling `json:"scaling,omitempty"`
-	MaxPods           *int32                     `json:"maxPods,omitempty"`
-	OsDiskType        *string                    `json:"osDiskType,omitempty"`
-	ScaleSetPriority  *string                    `json:"scaleSetPriority,omitempty"`
+	AdditionalTags       Tags                       `json:"additionalTags,omitempty"`
+	Mode                 string                     `json:"mode"`
+	SKU                  string                     `json:"sku"`
+	OSDiskSizeGB         *int32                     `json:"osDiskSizeGB,omitempty"`
+	AvailabilityZones    []string                   `json:"availabilityZones,omitempty"`
+	NodeLabels           map[string]string          `json:"nodeLabels,omitempty"`
+	Taints               Taints                     `json:"taints,omitempty"`
+	Scaling              *ManagedMachinePoolScaling `json:"scaling,omitempty"`
+	MaxPods              *int32                     `json:"maxPods,omitempty"`
+	OsDiskType           *string                    `json:"osDiskType,omitempty"`
+	OSType               *string                    `json:"osType,omitempty"`
+	EnableNodePublicIP   *bool                      `json:"enableNodePublicIP,omitempty"`
+	NodePublicIPPrefixID *string                    `json:"nodePublicIPPrefixID,omitempty"`
+	KubeletDiskType      *KubeletDiskType           `json:"kubeletDiskType,omitempty"`
+	LinuxOSConfig        *LinuxOSConfig             `json:"linuxOSConfig,omitempty"`
+	ScaleSetPriority     *string                    `json:"scaleSetPriority,omitempty"`
 }
+
+type Tags map[string]string
+
+type IdentityType string
+
+type KubeletDiskType string
+
+const (
+	KubeletDiskTypeOS        KubeletDiskType = "OS"
+	KubeletDiskTypeTemporary KubeletDiskType = "Temporary"
+)
+
+type LinuxOSConfig struct {
+	SwapFileSizeMB             *int32                     `json:"swapFileSizeMB,omitempty"`
+	Sysctls                    *SysctlConfig              `json:"sysctls,omitempty"`
+	TransparentHugePageDefrag  *TransparentHugePageOption `json:"transparentHugePageDefrag,omitempty"`
+	TransparentHugePageEnabled *TransparentHugePageOption `json:"transparentHugePageEnabled,omitempty"`
+}
+
+type SysctlConfig struct {
+	FsAioMaxNr                     *int32  `json:"fsAioMaxNr,omitempty"`
+	FsFileMax                      *int32  `json:"fsFileMax,omitempty"`
+	FsInotifyMaxUserWatches        *int32  `json:"fsInotifyMaxUserWatches,omitempty"`
+	FsNrOpen                       *int32  `json:"fsNrOpen,omitempty"`
+	KernelThreadsMax               *int32  `json:"kernelThreadsMax,omitempty"`
+	NetCoreNetdevMaxBacklog        *int32  `json:"netCoreNetdevMaxBacklog,omitempty"`
+	NetCoreOptmemMax               *int32  `json:"netCoreOptmemMax,omitempty"`
+	NetCoreRmemDefault             *int32  `json:"netCoreRmemDefault,omitempty"`
+	NetCoreRmemMax                 *int32  `json:"netCoreRmemMax,omitempty"`
+	NetCoreSomaxconn               *int32  `json:"netCoreSomaxconn,omitempty"`
+	NetCoreWmemDefault             *int32  `json:"netCoreWmemDefault,omitempty"`
+	NetCoreWmemMax                 *int32  `json:"netCoreWmemMax,omitempty"`
+	NetIpv4IPLocalPortRange        *string `json:"netIpv4IPLocalPortRange,omitempty"`
+	NetIpv4NeighDefaultGcThresh1   *int32  `json:"netIpv4NeighDefaultGcThresh1,omitempty"`
+	NetIpv4NeighDefaultGcThresh2   *int32  `json:"netIpv4NeighDefaultGcThresh2,omitempty"`
+	NetIpv4NeighDefaultGcThresh3   *int32  `json:"netIpv4NeighDefaultGcThresh3,omitempty"`
+	NetIpv4TCPFinTimeout           *int32  `json:"netIpv4TCPFinTimeout,omitempty"`
+	NetIpv4TCPKeepaliveProbes      *int32  `json:"netIpv4TCPKeepaliveProbes,omitempty"`
+	NetIpv4TCPKeepaliveTime        *int32  `json:"netIpv4TCPKeepaliveTime,omitempty"`
+	NetIpv4TCPMaxSynBacklog        *int32  `json:"netIpv4TCPMaxSynBacklog,omitempty"`
+	NetIpv4TCPMaxTwBuckets         *int32  `json:"netIpv4TCPMaxTwBuckets,omitempty"`
+	NetIpv4TCPTwReuse              *bool   `json:"netIpv4TCPTwReuse,omitempty"`
+	NetIpv4TCPkeepaliveIntvl       *int32  `json:"netIpv4TCPkeepaliveIntvl,omitempty"`
+	NetNetfilterNfConntrackBuckets *int32  `json:"netNetfilterNfConntrackBuckets,omitempty"`
+	NetNetfilterNfConntrackMax     *int32  `json:"netNetfilterNfConntrackMax,omitempty"`
+	VMMaxMapCount                  *int32  `json:"vmMaxMapCount,omitempty"`
+	VMSwappiness                   *int32  `json:"vmSwappiness,omitempty"`
+	VMVfsCachePressure             *int32  `json:"vmVfsCachePressure,omitempty"`
+}
+
+type TransparentHugePageOption string
+
+const (
+	TransparentHugePageOptionAlways       TransparentHugePageOption = "always"
+	TransparentHugePageOptionMadvise      TransparentHugePageOption = "madvise"
+	TransparentHugePageOptionNever        TransparentHugePageOption = "never"
+	TransparentHugePageOptionDefer        TransparentHugePageOption = "defer"
+	TransparentHugePageOptionDeferMadvise TransparentHugePageOption = "defer+madvise"
+)
